c8928b7b06b91cc3a9572d7782de333bcd26960b
gmegidish <gil@megidish.net>
2017-09-03 11:03:31 +0300

    Added TestFairy SDK to sample application, showing off various configurations

--- example/TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
+++ example/TestApplication/app/src/main/java/com/example/testapplication/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.testapplication;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.testfairy.TestFairy;
+
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		TestFairy.begin(this, "e27cf8c46bb25d8986e21915d700e493b268df0b"); 
 	}
 }
