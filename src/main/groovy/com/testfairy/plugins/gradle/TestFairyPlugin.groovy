8a79e64570c6504e7f38df31858f7f06b9305d3e
Diego Perini <diego@dperini.com>
2019-09-02 14:25:14 +0300

    Fixed travis

--- src/main/groovy/com/testfairy/plugins/gradle/TestFairyPlugin.groovy
+++ src/main/groovy/com/testfairy/plugins/gradle/TestFairyPlugin.groovy
@@ -8,6 +8,7 @@ import com.android.build.gradle.api.ApplicationVariant
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.TaskProvider
 
 class TestFairyPlugin implements Plugin<Project> {
 
